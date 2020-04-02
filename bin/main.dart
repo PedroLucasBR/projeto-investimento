09d3eb14210f49300e42e16d04e197f8215fca0c
Pedro Lucas Tavares da Silva <pedrolucastavaresdasilva@gmail.com>
2020-04-01 21:46:04 -0400

    Primeiro commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:projeto_investimento/projeto_investimento.dart' as projeto_investimento;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto_investimento.calculate()}!');
+}
